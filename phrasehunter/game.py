5d5570bd00c42e5f3aca615ee715f6372cdbb3cc
rjlanari <rjlanari@gmail.com>
2022-09-03 14:49:57 +0200

    Added project starter files

---
+++ phrasehunter/game.py
@@ -0,0 +1,13 @@
+# Create your Game class logic in here.
+
+
+
+# getting a random phrase, 
+
+# handling interactions, 
+
+# checking for a win/loss state, 
+
+# adding to the number missed.
+
+# starting the game, 
