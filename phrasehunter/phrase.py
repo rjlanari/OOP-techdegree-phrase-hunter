70169945601f678a08f495d4b30dc68a436fea7d
rjlanari <rjlanari@gmail.com>
2022-09-10 14:21:09 +0200

    worked on the class

--- phrasehunter/phrase.py
+++ phrasehunter/phrase.py
@@ -1,1 +1,29 @@
+
 # Create your Phrase class logic here.
+
+# Create the method display
+# Create the method check_letter
+# Create the method check_complete
+
+class Phrase:
+    def __init__(self, phrase):
+        self.phrase = phrase
+        self.guessed = False
+
+    def display(phrase):
+        disp_list = []
+        char_list = list(phrase)
+        for i in char_list:
+            if i == ' ':
+                disp_list.append(' ')
+            else:
+                disp_list.append('_')
+        print(''.join(disp_list))
+
+    def check_letter(self, input_letter):
+        return input_letter == self.phrase
+
+    def check_phrase(self, all_letters):
+        return all_letters == self.phrase
+
+#if __name__ == '__main__':
