0aba61570edaaffeed0dbd343e2f525ec5ff6a83
rjlanari <rjlanari@gmail.com>
2022-10-02 13:03:22 +0200

    Tested Finished Product

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from phrasehunter.game import Game
 
 if __name__ == '__main__':
-    game1 = Game()
-    game1.start_game()
-    #random_phrase = Phrase(game1.get_random_phrase())
-    #print(random_phrase)
-    #random_phrase.display('d')
-    #letters = game1.get_guess()
-    #print(letters)
+    while True:
+        game1 = Game()
+        game1.start_game()
+        if input('Do you want to play again? (Y/N) ').lower() not in ['y', 'yes']:
+            print("\n")
+            print("OK, see you next time!")
+            print("\n")
+            print("\n")
+            break
 
 
 
--- phrasehunter/game.py
+++ phrasehunter/game.py
@@ -1,16 +1,16 @@
+from curses.ascii import isalpha
 import random
-from re import X
-from phrase import Phrase
+from phrasehunter.phrase import Phrase
 
 class Game:
     def __init__(self):
         self.missed = 0
         self.phrases = [
                         "a bird in the hand is worth two in the bush",
-                        "an apple a day keeps the doctor away",
-                        "it takes two to tango",
-                        "like taking candy from a baby",
-                        "you can not make an omelet without breaking some eggs"
+                        "all tea all shade",
+                        "she already done had herses",
+                        "to be able to blend that is what realness is",
+                        "you can not make an ommelette without breaking some eggs"
                         ]
         self.active_phrase = None
         self.guesses = [] #list of letter provided by user
@@ -23,26 +23,38 @@ class Game:
         print("Hello, welcome to the Phrase Guessing Game!")
     
     def get_guess(self):
-        guess = input('Please, enter a letter that you think would be in the phrase!')              
+        while True:
+            guess = input('Please, enter a letter that you think would be in the phrase! ').lower()
+            print("\n")
+            if not isalpha(guess):
+                print('That is not a valid letter!')
+                print("\n")
+                continue
+            else:
+                break
+
         self.guesses.append(guess)
         return self.guesses
+
+        
     
     def game_over(self):
         if self.active_phrase.check_complete(self.guesses) == True:
             print("Congratulations, you've guessed the hidden phrase!")
+            print("\n")
         else:
             print('You did not find out the phrase, you lost!')
+            print("\n")
                 
 
     def start_game(self):
         game_running = True
+        print("\n")
         self.welcome() 
         self.active_phrase = Phrase(self.get_random_phrase()) 
-        print(str(self.active_phrase))  #just checking if the method worked
         self.active_phrase.display('')
         while game_running:
-            self.get_guess() #get a letter from user, giving tuple with guesses
-            print((self.guesses)) 
+            self.get_guess()
             if self.active_phrase.check_letter(self.guesses):
                 self.active_phrase.display(self.guesses)
                 if self.active_phrase.check_complete(self.guesses):
@@ -51,17 +63,11 @@ class Game:
                     game_running = True 
             else:
                 self.missed += 1
+                print(f'You have {5-self.missed} out of 5 lives remaining!')
+                print("\n")
                 if self.missed > 4:
                     game_running = False
         
         self.game_over()
-            
-if __name__ == '__main__':
 
-    game1 = Game()
-    game1.start_game()
-    #random_phrase = Phrase(game1.get_random_phrase())
-    #print(random_phrase)
-    #random_phrase.display('d')
-    #letters = game1.get_guess()
-    #print(letters)
+    
